bec41847c7bf6430cb8e9bad47ec050d40d4bbe0
Vaishnavi-531 <140788122+Vaishnavi-531@users.noreply.github.com>
2023-11-15 13:05:20 +0530

    Created Calculator Using Core Java

---
+++ Calculator.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = scanner.nextInt();
+
+        System.out.println("Select operation (+, -, *, /): ");
+        char operator = scanner.next().charAt(0);
+
+        int result = 0;
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Error: Division by zero is not allowed.");
+                }
+                break;
+            default:
+                System.out.println("Error: Invalid operator");
+        }
+
+        System.out.println("Result: " + result);
+    }
+}
